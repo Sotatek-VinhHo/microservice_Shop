35905cc9cde9bc5c82f5f715a7bb0e99a3b472ad
Sotatek-Vinhho <vinh.ho@sotatek.com>
2023-11-21 09:50:45 +0700

    sua loi response username

--- jwt/src/main/java/com/example/jwt/jwt_security/TokenProvider.java
+++ jwt/src/main/java/com/example/jwt/jwt_security/TokenProvider.java
@@ -1,14 +1,18 @@
+
 package com.example.jwt.jwt_security;
 
 import com.example.jwt.exceptionhandler.UnauthorizedException;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,52 +25,42 @@ public class TokenProvider {
 
     private static final Logger log = LoggerFactory.getLogger(TokenProvider.class);
 
+    private Key key;
+
     public String validateToken(String authToken) {
-//        if (authToken == null || !authToken.startsWith(prefix)) {
-//            log.info("JWT token is missing or invalid");
-//        }
-//
-//        String tokencheck = authToken.substring(7);
-//        try {
-//            parserBuilder().setSigningKey(AUTHORITIES_KEY).build().parseClaimsJws(tokencheck).getBody().getSubject();
-//
-//        } catch (JwtException | IllegalArgumentException e) {
-//            log.info("Invalid JWT token.");
-//            log.trace("Invalid JWT token trace.", e);
-//        }
-//
-//        return "true";
+
         if(authToken == null || !authToken.startsWith(prefix)) {
             throw new UnauthorizedException("JWT token is missing or invalid");
         }
 
-        String token = authToken.substring(7);
+        String token = authToken.split(" ")[1].trim();
 
         try {
-            Claims claims = Jwts.parserBuilder().setSigningKey(AUTHORITIES_KEY).build().parseClaimsJws(token).getBody();
-            System.out.println(claims);
-            String subject = claims.getSubject();
-            System.out.println(subject);
-            claims.get("username");
-            return Jwts.parserBuilder().setSigningKey(AUTHORITIES_KEY).build().parseClaimsJws(token).getBody().getSubject();
+            System.out.println(Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody());
+            return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getSubject();
         } catch (Exception e) {
             log.info(e.getMessage());
             throw new UnauthorizedException("JWT token is invalid");
         }
     }
 
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = new byte[0];
+        String secret = "this is secret this is secret this is secret this is secret this is secret this is secret this is secret this is secret";
+        keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
-
-    private static final String AUTHORITIES_KEY = "authhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh";
     static final long EXPIRATIONTIME = 36_000_000; // 10 phut 36_000_000
     public String createToken(String username) {
         System.out.println(username);
         Map<String,String> data = new HashMap<String,String>();
         data.put("username",username);
+
         return Jwts.builder()
                 .setSubject(username)
-                .setClaims(data)
-                .signWith(SignatureAlgorithm.HS256, AUTHORITIES_KEY)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
                 .compact();
     }
